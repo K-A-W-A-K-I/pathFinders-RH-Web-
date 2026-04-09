93c0046010dd337bcbcc5febd92843ca62dde03d
fadiboulehmi <fadi@boulehmi.com>
2026-04-09 15:38:28 +0100

    refactor: reorganize controllers into domain subfolders (User, Offre, Evenement, FichePaie, Reclamation, Other)

--- src/Controller/FichePaie/FichesPaiementController.php
+++ src/Controller/FichePaie/FichesPaiementController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\FichePaie;
 
 use App\Entity\FichesPaiement;
 use App\Form\FichesPaiementType;
--- src/Controller/Offre/CandidatureController.php
+++ src/Controller/Offre/CandidatureController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Offre;
 
 use App\Repository\CandidatRepository;
 use App\Repository\CandidatureRepository;
